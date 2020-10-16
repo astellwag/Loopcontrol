3441a9331113d0ed9d7e47aabb9a05e28564262d
Alex Stellwag <alex+github@stellwag.net>
2020-10-16 13:58:46 +0200

    first test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
 
+int numLoops = 1;
+int buttons[numLoops] = {8};
+int relayIn[numLoops] = {2};
+int relayOut[numLoops] = {5};
+int state[numLoops];
+int pressed[numLoops];
+
 void setup() {
-  // put your setup code here, to run once:
+  // setup I/O-ports
+  for(i=0; i<numLoops; i++){
+    pinMode(buttons[i], INPUT);
+    pinMode(relayIn[i], OUTPUT);
+    pinMode(relayOut[i], OUTPUT);
+
+    digitalWrite(relayIn[i], LOW);
+    digitalWrite(relayOut[i], LOW);
+
+    state[i] = LOW;
+    pressed[i] = false;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(i=0; i<numLoops; i++){
+    if(digitalRead(buttons[i])){
+      if(!pressed[i]){}
+        state[i] = !state[i];
+        digitalWrite(relayIn[i], state[i]);
+        digitalWrite(relayOut[i], state[i]);
+        pressed[i]=true;
+      }
+    } else {
+      pressed[i] = false;
+    } 
+  }
 }
